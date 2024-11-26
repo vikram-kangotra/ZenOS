8fc36bbbee9166242b93d4316da2a5d2ab178c82
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-11-27 02:23:16 +0530

    kprintf: Add 32-bit code for printing to screen

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,4 +1,5 @@
 #include "drivers/vga.h"
+#include "kernel/kprintf.h"
 
 void kmain() {
     
@@ -7,9 +8,7 @@ void kmain() {
     vga_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
     vga_clear_screen();
 
-    const char* str = "Welcome to ZenOS";
-    for (int i = 0; str[i] != '\0'; ++i) {
-        vga_write_char(str[i]);
-    }
+    kprintf("Welcome to ZenOS");
+
 
 }
---
+++ src/kernel/kprintf.c
@@ -0,0 +1,57 @@
+#include "drivers/vga.h"
+
+void kputchar(char ch) {
+    vga_write_char(ch);
+}
+
+void kprintf(const char* format, ...) {
+    uint32_t* args = (uint32_t*)&format + 1;
+
+    for (const char* p = format; *p != '\0'; p++) {
+        if (*p == '%' && *(p + 1) != '\0') {
+            p++; // Skip '%'
+            switch (*p) {
+                case 'c': {
+                    char ch = (char)(*args++);
+                    kputchar(ch);
+                    break;
+                }
+                case 's': {
+                    const char* str = (const char*)(*args++);
+                    while (*str) {
+                        kputchar(*str++);
+                    }
+                    break;
+                }
+                case 'd': {
+                    int value = (int)(*args++);
+                    if (value < 0) {
+                        kputchar('-');
+                        value = -value;
+                    }
+                    char buffer[10];
+                    int i = 0;
+                    do {
+                        buffer[i++] = '0' + value % 10;
+                        value /= 10;
+                    } while (value);
+                    while (i--) {
+                        kputchar(buffer[i]);
+                    }
+                    break;
+                }
+                case '%': {
+                    kputchar('%');
+                    break;
+                }
+                default: {
+                    kputchar('%');
+                    kputchar(*p);
+                    break;
+                }
+            }
+        } else {
+            kputchar(*p);
+        }
+    }
+}
