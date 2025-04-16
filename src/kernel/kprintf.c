98c9192e1d6c767ad6f144c6441efff8fdb31c68
Vikram Kangotra <vikramkangotra8055@gmail.com>
2025-04-17 00:19:22 +0530

    Add stdarg and improve printf

--- src/kernel/kprintf.c
+++ src/kernel/kprintf.c
@@ -1,18 +1,14 @@
 #include "drivers/vga.h"
 #include "drivers/serial.h"
 #include "kernel/kprintf.h"
+#include "stdarg.h"
 
 void kputchar(char ch) {
     vga_write_char(ch);
     serial_write_char(ch);
 }
 
-#define GET_ARG(type, reg_index, stack_pointer)                \
-    ((reg_index < 4) ? ((type)(reg_args[reg_index++])) :       \
-                       ((type)(*stack_pointer++)))
-
 void kprintf(enum LogLevel level, const char* format, ...) {
-
     switch (level) {
         case INFO: vga_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK); break;
         case DEBUG: vga_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK); break;
@@ -20,37 +16,27 @@ void kprintf(enum LogLevel level, const char* format, ...) {
         case ERROR: vga_set_color(PRINT_COLOR_LIGHT_RED, PRINT_COLOR_BLACK); break;
     }
 
-    uintptr_t reg_args[4];
-    asm volatile (
-        "mov %%rdx, %0\n\t"
-        "mov %%rcx, %1\n\t"
-        "mov %%r8,  %2\n\t"
-        "mov %%r9,  %3\n\t"
-        : "=m"(reg_args[0]), "=m"(reg_args[1]), 
-          "=m"(reg_args[2]), "=m"(reg_args[3])
-    );
-
-    uintptr_t* stack_args = (uintptr_t*)__builtin_frame_address(0) + 2;
-    int reg_index = 0;
+    va_list args;
+    va_start(args, format);
 
     for (const char* p = format; *p != '\0'; p++) {
         if (*p == '%' && *(p + 1) != '\0') {
             p++; // Skip '%'
             switch (*p) {
                 case 'c': {
-                    char ch = GET_ARG(char, reg_index, stack_args);
+                    char ch = va_arg(args, int);
                     kputchar(ch);
                     break;
                 }
                 case 's': {
-                    const char* str = GET_ARG(const char*, reg_index, stack_args);
+                    const char* str = va_arg(args, const char*);
                     while (*str) {
                         kputchar(*str++);
                     }
                     break;
                 }
                 case 'd': {
-                    int value = GET_ARG(int, reg_index, stack_args);
+                    int value = va_arg(args, int);
                     if (value < 0) {
                         kputchar('-');
                         value = -value;
@@ -67,7 +53,7 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     break;
                 }
                 case 'u': {
-                    unsigned int value = GET_ARG(unsigned int, reg_index, stack_args);
+                    unsigned int value = va_arg(args, unsigned int);
                     char buffer[10];
                     int i = 0;
                     do {
@@ -80,7 +66,7 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     break;
                 }
                 case 'x': {
-                    unsigned int value = GET_ARG(unsigned int, reg_index, stack_args);
+                    unsigned int value = va_arg(args, unsigned int);
                     char buffer[8];
                     int i = 0;
                     do {
@@ -94,7 +80,7 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     break;
                 }
                 case 'p': {
-                    uintptr_t ptr = GET_ARG(uintptr_t, reg_index, stack_args);
+                    uintptr_t ptr = va_arg(args, uintptr_t);
                     kputchar('0'); kputchar('x');
                     char buffer[16];
                     int i = 0;
@@ -109,7 +95,7 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     break;
                 }
                 case 'o': {
-                    unsigned int value = GET_ARG(unsigned int, reg_index, stack_args);
+                    unsigned int value = va_arg(args, unsigned int);
                     char buffer[11];
                     int i = 0;
                     do {
@@ -122,7 +108,7 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     break;
                 }
                 case 'b': {
-                    unsigned int value = GET_ARG(unsigned int, reg_index, stack_args);
+                    unsigned int value = va_arg(args, unsigned int);
                     char buffer[32];
                     int i = 0;
                     do {
@@ -138,15 +124,15 @@ void kprintf(enum LogLevel level, const char* format, ...) {
                     kputchar('%');
                     break;
                 }
-                default: {
-                    kputchar('%');
+                default:
                     kputchar(*p);
                     break;
-                }
             }
         } else {
             kputchar(*p);
         }
     }
+
+    va_end(args);
 }
 
